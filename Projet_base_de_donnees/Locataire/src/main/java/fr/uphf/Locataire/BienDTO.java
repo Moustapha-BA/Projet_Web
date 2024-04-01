bece52f7bcc6149287942b6d80ad7e59154adc54
Ghazi <gbouagila@gmail.com>
2024-04-01 17:18:49 +0200

    changes

---
+++ Projet_base_de_donnees/Locataire/src/main/java/fr/uphf/Locataire/BienDTO.java
@@ -0,0 +1,15 @@
+package fr.uphf.Locataire;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BienDTO {
+    private Long id;
+    private String adresse;
+    private String type;
+    private int surface;
+    private int nbPieces;
+    private int loyer;
+}
