f4cc54e65cfac9d2288bff57d35020966b90b35d
Moustapha <moustaphaba1214@gmail.com>
2024-04-12 20:38:54 +0200

    Commit -

--- Projet_base_de_donnees/bienImmobilier/src/main/java/fr/uphf/bienImmobilier/resources/BienRepository.java
+++
@@ -1,4 +0,0 @@
-package fr.uphf.bienImmobilier.resources;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface BienRepository extends JpaRepository<BienImmobilier, Long>{
-}
