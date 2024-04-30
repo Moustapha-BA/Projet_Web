fe2b2dd101f49609b124718d1c3c4dd3e5368eea
Ghazi <gbouagila@gmail.com>
2024-04-30 21:47:18 +0200

    Dernier modification - avec configuration RabbitMQ

--- Projet_base_de_donnees/Locataire/src/main/java/fr/uphf/Locataire/model/locataireControlleur.java
+++ Projet_base_de_donnees/Locataire/src/main/java/fr/uphf/Locataire/model/locataireControlleur.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
 import java.util.List;
 
 @RestController
