c06422cd6c16d51feb06af7e64e07c5ad7c4563f
Moustapha <moustaphaba1214@gmail.com>
2024-04-30 22:15:03 +0200

    Commit - Sauvegarde

---
+++ Projet_base_de_donnees/Locataire/src/main/java/fr/uphf/Locataire/config/RabbitMQConfig.java
@@ -0,0 +1,51 @@
+package fr.uphf.Locataire.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE = "bienImmo-queue";
+    public static final String EXCHANGE = "bienImmo-exchange";
+    public static final String ROUTING_KEY = "bienImmo-key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+
+    @Bean
+    public Jackson2JsonMessageConverter consumerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(consumerJackson2MessageConverter());
+        return factory;
+    }
+
+}
+
---
+++ Projet_base_de_donnees/bienImmo - Copie/src/main/java/fr/uphf/bienImmo/resources/BienImmoController.java
@@ -0,0 +1,70 @@
+package fr.uphf.bienImmo.resources;
+
+import fr.uphf.bienImmo.services.BienImmoApiService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/biensImmo")
+public class BienImmoController {
+
+    private BienImmoApiService bienImmoApiService;
+
+    public BienImmoController(BienImmoApiService bienImmoApiService) {
+        this.bienImmoApiService = bienImmoApiService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BienImmo>> listerBiensImmo() {
+        return ResponseEntity.ok(this.bienImmoApiService.listerBiensImmo());
+    }
+
+
+    //méthode GetMapping pour lister un bienImmo par son id
+    @GetMapping("/{idBienImmo}")
+    public ResponseEntity<BienImmo> recupererBienImmo(@PathVariable("idBienImmo") Long idBienImmo) {
+        try {
+            return ResponseEntity.ok(this.bienImmoApiService.recupererBienImmoById(idBienImmo));
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //méthode ajouter un BienImmo
+    @PostMapping
+    public ResponseEntity ajouterBienImmo(@RequestBody CreationBienImmoRequestODT creationBienImmoRequestODT) {
+        try {
+            return ResponseEntity.ok(this.bienImmoApiService.ajouterBienImmo(creationBienImmoRequestODT));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erreur lors de l'ajout du bienImmo");
+        }
+    }
+
+    //méthode pour modifier un BienImmo
+    @PutMapping("/{idBienImmo}")
+    public ResponseEntity modifierBienImmo(@PathVariable("idBienImmo") Long idBienImmo, @RequestBody CreationBienImmoResponseODT creationBienImmoResponseODT) {
+        try {
+            return ResponseEntity.ok(this.bienImmoApiService.modifierBienImmo(idBienImmo, creationBienImmoResponseODT));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erreur lors de la modification du bienImmo");
+        }
+    }
+
+     //Ajout d'un API pour lister less biensImmos d'un locataire
+    @GetMapping("/locataires/{idLocataire}/biens")
+    public ResponseEntity<List<BienImmoDTO>> listerBiensImmoParLocataireId(@PathVariable("idLocataire") Long idLocataire) {
+        try {
+            List<BienImmoDTO> biens = bienImmoApiService.listerBiensImmoParLocataire(idLocataire);
+            if (biens.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.ok(biens);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+
+}
